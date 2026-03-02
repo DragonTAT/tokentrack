a7343809070f7953260b936d9c0af80a5fe6236c
Yingchuan Chen <yingchuan3333@gmail.com>
2026-03-02 02:23:47 -0700

    chore: remove test scripts

--- apps/TokscaleMac/test_url.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-let arg = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : ".build/debug/TokscaleMac"
-let url = URL(fileURLWithPath: arg)
-let execDir = url.deletingLastPathComponent()
-let resolved = execDir.appendingPathComponent("../../../TokscaleMac/Resources/tokscale").standardized.path
-print("Input: \(arg)")
-print("URL: \(url.path)")
-print("ExecDir: \(execDir.path)")
-print("Resolved: \(resolved)")
--- apps/TokscaleMac/test_url2.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-let execDir = URL(fileURLWithPath: "/Users/chen/Desktop/claude/tokentrack/apps/TokscaleMac/.build/debug")
-let paths = [
-    execDir.appendingPathComponent("../../../TokscaleMac/Resources/tokscale").standardized.path,
-    execDir.deletingLastPathComponent().deletingLastPathComponent().appendingPathComponent("TokscaleMac/Resources/tokscale").path
-]
-print(paths)
